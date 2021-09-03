f2f5a786b11823995c569871790e7864959665af
dmariogatto <d.mario.gatto@gmail.com>
2021-09-04 09:04:18 +0930

    Update update_ryzen_chipset.sh

--- amd-ryzen-chipset/update_ryzen_chipset.sh
+++ amd-ryzen-chipset/update_ryzen_chipset.sh
@@ -10,8 +10,16 @@ uninstallScript="$baseDir/tools/chocolateyuninstall.ps1"
 nuspec="$baseDir/amd-ryzen-chipset.nuspec"
 
 currentUrl=$(grep '$url =' $installScript | cut -d \' -f 2)
-currentVersion=$(echo $currentUrl | sed 's/.*software_\(.*\).exe.*/\1/')
 currentChecksum=$(grep '$checksum =' $installScript | cut -d \' -f 2)
+currentVersion=""
+
+if [[ "$newUrl" == *'.exe'* ]]; then
+    echo "Is exe"
+    currentVersion=$(echo $currentUrl | sed 's/.*_\(.*\).exe.*/\1/')
+elif [[ "$newUrl" == *'.zip'* ]]; then
+    echo "Is zip"
+    newVersion=$(echo $currentUrl | sed 's/.*_\(.*\).zip.*/\1/')
+fi
 
 echo "Current Url: $currentUrl"
 echo "Current Version: $currentVersion"
