984e3f0ac010aa0b31b9672bd05c34a1a7652f89
GitHub Action <action@github.com>
2026-07-31 09:14:44 +0000

    update ryzen chipset '2.17.25.506'

--- amd-ryzen-chipset/tools/chocolateyinstall.ps1
+++ amd-ryzen-chipset/tools/chocolateyinstall.ps1
@@ -11,8 +11,8 @@ else {
     $installerFilePath = "$toolsDir\$installerFileName"
     $unzipDir = "$toolsDir\amd_chipset_drivers"
 
-    $url = 'https://drivers.amd.com/drivers/amd_chipset_software_8.05.04.516.exe'
-    $checksum = '1609fbf1375ca1f25e461d1de7d21da096d018624d8b63dfc3d81cb6a8bde3ad'
+    $url = 'https://drivers.amd.com/drivers/AMD_Chipset_Software_Win7_2.17.25.506.exe'
+    $checksum = '67ec549946f804d5d764ca042d2572465194bdcde453ca7ab80829445d1c7d0a'
 
     $downloadFilePath = $installerFilePath
 
